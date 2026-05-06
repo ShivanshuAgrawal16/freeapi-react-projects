246b77d02606778b5dd804f584d21ce0654a0753
ShivanshuAgrawal16 <shivanshu483501@gmail.com>
2026-05-06 22:59:49 +0530

    Proj: init

---
+++ freeapi-jokes/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+
+const API_URL = "https://api.freeapi.app/api/v1/public/randomjokes/joke/random";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl text-orange-500">Jokes API Test</h1>
+    </div>
+  );
+}
+
+export default App;
